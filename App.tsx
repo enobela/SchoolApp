a9db32c396a1c5374f2263d911d0cf36ea68c78a
RC_Student_lab <enobela@rosebankcollege.co.za>
2024-09-27 18:19:29 +0000

    Second commit new design

--- App.tsx
+++ App.tsx
@@ -2,8 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
+const DATA =[
+  {
+    sname:'Ephraim',
+    modulecode:'MAST5112',
+  },
+  {
+    sname:'Musa',
+    modulecode:'IMAD5112', 
+  },
+  {
+    sname:'John',
+    modulecode:'IMAD5112', 
+  }
+  ];
+  
+  type ItemsProps = {sname: string};
+
+
+  const Item =({sname}:ItemsProps) =>(
+    <View>
+      <Text>{sname}</Text>
+    </View>
+  );
 export default function App() {
 
 // Variables 
@@ -13,35 +37,89 @@ const [studentName, setStudentName]= useState('');
   return (
     
     <View style={styles.container}>
+      <View>
+        <Text style={styles.AppHome}>School App</Text>
+      </View>
+      <View>
+    
+<Text style={styles.Textuser}>Student Name</Text>
+<TextInput style={styles.formInput} placeholder=''onChangeText={newname=>setStudentName(newname)}/>
+<Text style={styles.Textuser}>Module Name</Text>
+<TextInput style={styles.formInput} placeholder=''onChangeText={newText=>setModuleName(newText)}/>
+<View style={styles.buttonClick}>
+<Button   title='Add' onPress={()=>{
+  alert(studentName +" "+ ModuleName)}} />
+</View>
 
-      <Text style={styles.welcomepage}>Home Page</Text>
-      <Text style={styles.welcomepage}>Student Name</Text>
-      <TextInput placeholder='Student Name'
-      
-      onChangeText={newname=>setStudentName(newname)}
-      
-      />
-      <Text style={styles.welcomepage}>Module Name</Text>
-      <TextInput placeholder='Module Name'
-      
-      onChangeText={newText=>setModuleName(newText)}
-      
-      />
-      <Button title='Add' onPress={()=>{
-        alert(studentName +" "+ ModuleName)}}/>
+
+ </View>
+<View style={styles.Listcontainer}>
+  <Text style={styles.moduleList}>Students List</Text>
+  <SafeAreaView>
+  <FlatList data={DATA}
+  renderItem={({item}) => <Item sname={item.sname}/>}
+  
+  />
+  </SafeAreaView>
+  
+  <Text>Total {DATA.length}</Text>
+  
+</View>
+     
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
+  AppHome :{
+   marginTop:35,
+   margin:5,
+   
+   
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#d6dee2',
+    padding:10,
+    marginTop:10,
+    position:'relative',
+    margin:5,
   },
-  welcomepage :{
-    color:'red'
-  }
+  Listcontainer :{
+    position:'relative',
+    margin:15
+  },
+moduleList :{
+  fontSize:24,
+  fontWeight:'bold',
+  margin:4,
+},
+  welcomepage: {
+    color:'red',
+    fontSize:20,
+  },
+
+formInput:{
+ backgroundColor:'grey',
+ borderColor:'lightgreen',
+ borderRadius:6,
+ textAlign:'center',
+ fontSize:23,
+ 
+  },
+Textuser: {
+fontSize:28,
+fontWeight:'bold',
+margin:5,
+marginTop:5,
+color:'#77b0cd'
+
+},
+buttonClick: {
+marginTop:12,
+borderRadius:6,
+}
+
 });
