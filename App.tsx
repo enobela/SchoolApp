18b4b23a647aa307f3b84f321f868bd5ad7a9683
RC_Student_lab <enobela@rosebankcollege.co.za>
2024-09-27 15:30:00 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+// Variables 
+const [ModuleName, setModuleName]= useState('');
+const [studentName, setStudentName]= useState('');
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.welcomepage}>Home Page</Text>
+      <Text style={styles.welcomepage}>Student Name</Text>
+      <TextInput placeholder='Student Name'
+      
+      onChangeText={newname=>setStudentName(newname)}
+      
+      />
+      <Text style={styles.welcomepage}>Module Name</Text>
+      <TextInput placeholder='Module Name'
+      
+      onChangeText={newText=>setModuleName(newText)}
+      
+      />
+      <Button title='Add' onPress={()=>{
+        alert(studentName +" "+ ModuleName)}}/>
     </View>
+    
   );
 }
 
@@ -17,4 +41,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcomepage :{
+    color:'red'
+  }
 });
